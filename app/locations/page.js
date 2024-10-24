2269a79c1f05098edd49eb61ea18947c8eae132f
YOUR NAMEcls <faiza.prism@gmail.com>
2024-10-24 17:22:37 +0500

    add location api

--- app/locations/page.js
+++ app/locations/page.js
@@ -6,6 +6,8 @@ import FilterSection from "../components/locations/filter-section/FilterSection"
 import LocationsList from "../components/locations/locations-list/LocationsList";
 import ContactSection from "../components/home/contact-section/ContactSection";
 import FAQSection from "../components/home/faq-section/FAQSection";
+// api
+import { fatchProjectList } from "../apis/commonApi";
 // data
 import projectsData from "../db/projectsData";
 // img
@@ -14,6 +16,26 @@ import bannerImg from "../assets/banner/locationsbanner.webp";
 const Locations = () => {
   const [locationData, setLocationData] = useState([]);
   const [filters, setFilters] = useState({ zone: "zone-1" });
+  const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const fetchProjectListData = async () => {
+  //     try {
+  //       setIsLoading(true); // Show the loader
+  //       const { data } = await fatchProjectList();
+  //       const locationsList = data?.filter((project) => {
+  //         // Apply the default zone filter or user-selected zone
+  //         return project?.location?.zone?.route === filters.zone;
+  //       });
+  //       setLocationData(locationsList);
+  //     } catch (error) {
+  //       console.error("Error fetching Data:", error);
+  //     } finally {
+  //       setIsLoading(false); // Hide the loader
+  //     }
+  //   };
+  //   fetchProjectListData();
+  // }, [filters.zone]);
 
   useEffect(() => {
     const locationsList = projectsData?.filter((project) => {
